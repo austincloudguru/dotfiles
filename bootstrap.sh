c27c885e7a7dbac79c0217816d0b16d1ea566cdc
Mark Honomichl <mars@marsdominion.com>
2017-05-11 21:12:59 -0500

    Fixed LICENSE

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ function doIt() {
         --exclude ".idea/" \
         --exclude "bootstrap.sh" \
         --exclude "README.md" \
-        --exclude "LICENSE.txt" \
+        --exclude "LICENSE" \
         -avh --no-perms . ~;
     source ~/.bash_profile;
 }
