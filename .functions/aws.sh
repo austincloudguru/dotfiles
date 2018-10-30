c53bd7e00006a41b490a9986a3a93ba67ec508c8
Mark Honomichl <mark@austincloud.guru>
2018-10-30 18:03:04 -0500

    Added av alias

---
+++ .functions/aws.sh
@@ -0,0 +1,6 @@
+# THIS FILE IS UNDER VERSION CONTROL.  MAKE CHANGES IN YOUR REPO!!!!! #
+#*********************************************************************#
+#!/usr/bin/env bash
+
+# Alias aws-vault
+alias av='aws-vault'
