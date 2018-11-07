e425164f06a6e708f4449c547406c834291f8863
Mark Honomichl <mark@austincloud.guru>
2018-11-06 19:05:18 -0600

    Added acg-awsmaster function

--- .functions/aws.sh
+++ .functions/aws.sh
@@ -5,7 +5,16 @@
 # Alias aws-vault
 alias av='aws-vault'
 
-# Log into aws2
+# Log into acg-awsmaster AWS Account
+acgm() {
+  if [[ -z $OP_SESSION_marsdominion ]]; then
+   eval $(op signin marsdominion)
+  fi
+  aws-vault exec -m $(op get totp "AWS - acg-awsmaster") acg2
+  unset OP_SESSION_marsdominion
+}
+
+# Log into acg-aws2 AWS Account
 acg2() {
   if [[ -z $OP_SESSION_marsdominion ]]; then
    eval $(op signin marsdominion)
