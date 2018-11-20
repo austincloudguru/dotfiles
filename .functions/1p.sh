ebb9f8336c4e86b99e45f9f9b4c812ec5da1be0c
Mark Honomichl <mark@austincloud.guru>
2018-11-20 10:18:51 -0600

    Added new function and rearranged login/logout

--- .functions/1p.sh
+++ .functions/1p.sh
@@ -1,12 +1,25 @@
 # THIS FILE IS UNDER VERSION CONTROL.  MAKE CHANGES IN YOUR REPO!!!!! #
 #*********************************************************************#
 #!/usr/bin/env bash
+
+opsignin() {
+  if [[ -z $OP_SESSION_marsdominion ]]; then
+    eval $(op signin marsdominion)
+  fi
+}
+
+opsignout() {
+  unset OP_SESSION_marsdominion
+}
+
+sshkey() {
+  opsignin
+  echo "$(op get item "acg-master" |jq -r '.details.notesPlain')"|ssh-add -
+  opsignout
+}
+
 gittoken() {
-
-if [[ -z $OP_SESSION_marsdominion ]]; then
-   eval $(op signin marsdominion)
-fi
-GIT_TOKEN=$(op get item "GitHub"|jq -r '.details.sections[] | select(.fields).fields[] | select(.t== "Personal Laptop").v')
-
-unset OP_SESSION_marsdominion
+  opsignin
+  GIT_TOKEN=$(op get item "GitHub"|jq -r '.details.sections[] | select(.fields).fields[] | select(.t== "Personal Laptop").v')
+  opsignout
 }
