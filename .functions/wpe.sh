9f1c02d6875e51bfefb4eae5bd31cabfa20a27e0
Mark Honomichl <mark@austincloud.guru>
2019-04-01 12:46:30 -0500

    Removed the old wpe commands

--- .functions/wpe.sh
+++ .functions/wpe.sh
@@ -1,33 +1,3 @@
-wpe-users() {
-  opon
-  aws-vault exec -t 4h --assume-role-ttl=1h -m $(getmfa "AWS - WPE Users") wpe-users
-  opoff
-}
-
-wpe-dev() {
-  opon
-  aws-vault exec -t 4h --assume-role-ttl=1h -m $(getmfa "AWS - WPE Users") wpe-dev
-  opoff
-}
-
-wpe-prod() {
-  opon
-  aws-vault exec -t 4h --assume-role-ttl=1h -m $(getmfa "AWS - WPE Users") wpe-prod
-  opoff
-}
-
-wpe-corp() {
-  opon
-  aws-vault exec -t 4h --assume-role-ttl=1h -m $(getmfa "AWS - WPE Users") wpe-corp
-  opoff
-}
-
-wpe-pur() {
-  opon
-  aws-vault exec -t 4h --assume-role-ttl=1h -m $(getmfa "AWS - WPE Users") wpe-pur
-  opoff
-}
-
 wpe() {
   opon
   aws-vault exec -t 4h --assume-role-ttl=1h -m $(getmfa "AWS - WPE Users") wpe-$1
