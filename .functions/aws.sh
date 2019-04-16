215a316eee6dbde231fbb94a23932856d1c40350
Mark Honomichl <mark@austincloud.guru>
2019-04-16 14:39:11 -0500

    Added some disk cleanup stuff

--- .functions/aws.sh
+++ .functions/aws.sh
@@ -54,3 +54,41 @@ acg() {
   aws-vault exec -t 4h --assume-role-ttl=1h -m $(getmfa "aws-acg-master") acg-$1
 
 }
+
+# List Available Volumes
+aws-av() {
+  if [ -z "$1" ]; then
+   echo "Need to include the number of days to list"
+  else
+    for region in `aws ec2 describe-regions --output text | cut -f 2|cut -d . -f 2`;
+    do 
+      echo "-- $region"; 
+        aws ec2 describe-volumes --region $region --filters "Name=status,Values=available" \
+          --query "Volumes[?CreateTime<='$(date -v -${1}d +%Y-%m-%d)'].VolumeId" \
+          --output text        
+      echo ;
+    done
+  fi
+}
+
+# Cleanup Available Volumes
+aws-avc() {
+  if [ -z "$1" ]; then
+   echo "Needs number of days old to clean up"
+  else
+    for region in `aws ec2 describe-regions --output text | cut -f 2|cut -d . -f 2`;
+    do 
+      echo "-- $region"; 
+      for a in `aws ec2 describe-volumes \
+        --region $region \
+        --filters "Name=status,Values=available" \
+        --query "Volumes[?CreateTime<='$(date -v -3d +%Y-%m-%d)'].VolumeId" \
+        --output text`
+      do
+        echo "Deleting $a..."
+        aws ec2 delete-volume --region $region --volume-id $a
+      done        
+      echo ;
+    done
+  fi
+}
