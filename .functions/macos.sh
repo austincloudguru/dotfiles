f1f7dd3503273d0fe73372021978d38a897baaa5
Mark Honomichl <mark@austincloud.guru>
2019-09-04 10:43:33 -0500

    Removed odrive tool

--- .functions/macos.sh
+++ .functions/macos.sh
@@ -12,12 +12,6 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
       export PIP_REQUIRE_VIRTUALENV=true
     }
 
-    # odrive sync tool
-    if [ -d ~/.odrive/bin ]; then
-      OD_VERSION=$(ls ~/.odrive/bin)
-      alias od='~/.odrive/bin/${OD_VERSION}/odrive.py'
-    fi
-
     alias abs='ssh abs.austincloud.guru -t tmuxinator bastion'
 
 fi
