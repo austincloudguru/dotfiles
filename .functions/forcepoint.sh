356b40e924e734995c95ecf75f8c41346dfc0216
Mark Honomichl <mark@austincloud.guru>
2019-10-09 15:11:08 -0500

    Fixed the shell

--- .functions/forcepoint.sh
+++ .functions/forcepoint.sh
@@ -1,18 +1,18 @@
 # THIS FILE IS UNDER VERSION CONTROL.  MAKE CHANGES IN YOUR REPO!!!!! #
 #*********************************************************************#
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 # AWS CDP-DEV account
 cdp-dev() {
-  aws-okta exec cdp-dev -- /bin/bash
+  aws-okta exec cdp-dev -- /bin/zsh
 }
 
 # AWS CDP-DEV account
 cdp-cicd() {
-  aws-okta exec cdp-cicd -- /bin/bash
+  aws-okta exec cdp-cicd -- /bin/zsh
 }
 
 # AWS fp-cloudsec-main account
 cdp-main() {
-  aws-okta exec fp-cloudsec-main -- /bin/bash
+  aws-okta exec fp-cloudsec-main -- /bin/zsh
 }
