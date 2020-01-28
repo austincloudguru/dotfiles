b26bd6ae6f681b652b55c2b10ae1efed8797ab2b
Mark Honomichl <mark@austincloud.guru>
2020-01-28 15:40:37 -0600

    Added a checker for all repos

--- .functions/git.sh
+++ .functions/git.sh
@@ -27,3 +27,22 @@ if [ -f /usr/local/bin/hub ]
 then
   alias git='hub'
 fi
+
+gitgood() {
+  cd ~/source
+  RED=$(tput setaf 1)
+  GREEN="\033[0;32m"
+  RESET=$(tput sgr0)
+  for git_repo in `ls -1 -d */*`;
+  do
+  BRANCH=$(git --git-dir=./$git_repo/.git --work-tree=./$git_repo branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/*\(.*\)/\1/')
+  printf "%-40s %-10s %-4s" "$git_repo" "$BRANCH"
+  if [ ! -z "$(git --git-dir=./$git_repo/.git --work-tree=./$git_repo status --short)" ]; then
+      printf " ${RED}✗"
+    else
+      printf " ${GREEN}✔"
+    fi
+  printf "\n${RESET}"
+  done
+  cd -
+}
